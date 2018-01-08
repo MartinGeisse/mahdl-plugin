a58937d1f94e9348978e49ce1d69f639dff3315f
Martin Geisse <martingeisse@googlemail.com>
2018-01-08 21:44:04 +0100

    safe delete support

---
+++ src/name/martingeisse/mahdl/plugin/input/MahdlRefactoringSupportProvider.java
@@ -0,0 +1,21 @@
+package name.martingeisse.mahdl.plugin.input;
+
+import com.intellij.lang.refactoring.RefactoringSupportProvider;
+import com.intellij.psi.PsiElement;
+import name.martingeisse.mahdl.plugin.input.psi.ImplementationItem_ModuleInstance;
+import name.martingeisse.mahdl.plugin.input.psi.Module;
+import name.martingeisse.mahdl.plugin.input.psi.PortDefinition;
+import name.martingeisse.mahdl.plugin.input.psi.SignalLikeDefinition;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ *
+ */
+public class MahdlRefactoringSupportProvider extends RefactoringSupportProvider {
+
+	@Override
+	public boolean isSafeDeleteAvailable(@NotNull PsiElement element) {
+		return (element instanceof Module) || (element instanceof PortDefinition) || (element instanceof SignalLikeDefinition) || (element instanceof ImplementationItem_ModuleInstance);
+	}
+
+}
