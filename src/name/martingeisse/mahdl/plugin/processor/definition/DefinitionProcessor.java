544d48ca6430e93b28a505c469435073bb4ba4b9
Martin Geisse <martingeisse@googlemail.com>
2018-02-02 08:27:23 +0100

    module processor

--- src/name/martingeisse/mahdl/plugin/processor/definition/DefinitionProcessor.java
+++ src/name/martingeisse/mahdl/plugin/processor/definition/DefinitionProcessor.java
@@ -9,6 +9,8 @@ import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import name.martingeisse.mahdl.plugin.input.psi.*;
 import name.martingeisse.mahdl.plugin.processor.ErrorHandler;
 import name.martingeisse.mahdl.plugin.processor.expression.ConstantValue;
+import name.martingeisse.mahdl.plugin.processor.expression.ExpressionProcessor;
+import name.martingeisse.mahdl.plugin.processor.expression.ProcessedExpression;
 import name.martingeisse.mahdl.plugin.processor.type.DataTypeProcessor;
 import name.martingeisse.mahdl.plugin.processor.type.ProcessedDataType;
 import org.jetbrains.annotations.NotNull;
@@ -26,11 +28,15 @@ public final class DefinitionProcessor {
 
 	private final ErrorHandler errorHandler;
 	private final DataTypeProcessor dataTypeProcessor;
+	private final ExpressionProcessor expressionProcessor;
 	private final Map<String, Named> definitions;
 
-	public DefinitionProcessor(@NotNull ErrorHandler errorHandler, @NotNull DataTypeProcessor dataTypeProcessor) {
+	public DefinitionProcessor(@NotNull ErrorHandler errorHandler,
+							   @NotNull DataTypeProcessor dataTypeProcessor,
+							   @NotNull ExpressionProcessor expressionProcessor) {
 		this.errorHandler = errorHandler;
 		this.dataTypeProcessor = dataTypeProcessor;
+		this.expressionProcessor = expressionProcessor;
 		this.definitions = new HashMap<>();
 	}
 
@@ -55,65 +61,59 @@ public final class DefinitionProcessor {
 		List<Named> result = new ArrayList<>();
 		if (implementationItem instanceof ImplementationItem_SignalLikeDefinitionGroup) {
 			ImplementationItem_SignalLikeDefinitionGroup signalLike = (ImplementationItem_SignalLikeDefinitionGroup) implementationItem;
+			SignalLikeKind kind = signalLike.getKind();
+			DataType dataType = signalLike.getDataType();
+			ProcessedDataType processedDataType = dataTypeProcessor.processDataType(dataType);
+			for (SignalLikeDefinition signalLikeDefinition : signalLike.getDefinitions().getAll()) {
 
-			// TODO merge these two branches
-			if (signalLike.getKind() instanceof SignalLikeKind_Constant) {
-				DataType dataType = signalLike.getDataType();
-				ProcessedDataType processedDataType = dataTypeProcessor.processDataType(dataType);
-				for (SignalLikeDefinition signalLikeDefinition : signalLike.getDefinitions().getAll()) {
-					PsiElement nameElement;
-					ExtendedExpression initializer;
+				// extract name element and initializer
+				PsiElement nameElement;
+				ExtendedExpression initializer;
+				if (signalLikeDefinition instanceof SignalLikeDefinition_WithoutInitializer) {
+					SignalLikeDefinition_WithoutInitializer typedDeclaredSignalLike = (SignalLikeDefinition_WithoutInitializer) signalLikeDefinition;
+					nameElement = typedDeclaredSignalLike.getIdentifier();
+					initializer = null;
+				} else if (signalLikeDefinition instanceof SignalLikeDefinition_WithInitializer) {
+					SignalLikeDefinition_WithInitializer typedDeclaredSignalLike = (SignalLikeDefinition_WithInitializer) signalLikeDefinition;
+					nameElement = typedDeclaredSignalLike.getIdentifier();
+					initializer = typedDeclaredSignalLike.getInitializer();
+				} else {
+					errorHandler.onError(signalLikeDefinition, "unknown PSI node");
+					continue;
+				}
+
+				// build the definition
+				Named definition;
+				if (kind instanceof SignalLikeKind_Constant) {
+					Constant constant = new Constant(nameElement, dataType, processedDataType, initializer);
 					ConstantValue value;
-					if (signalLikeDefinition instanceof SignalLikeDefinition_WithoutInitializer) {
-
-						SignalLikeDefinition_WithoutInitializer typedDeclaredSignalLike = (SignalLikeDefinition_WithoutInitializer) signalLikeDefinition;
+					if (initializer == null) {
 						errorHandler.onError(signalLikeDefinition, "constant must have an initializer");
-						nameElement = typedDeclaredSignalLike.getIdentifier();
-						initializer = null;
 						value = ConstantValue.Unknown.INSTANCE;
-
-					} else if (signalLikeDefinition instanceof SignalLikeDefinition_WithInitializer) {
-
-						SignalLikeDefinition_WithInitializer typedDeclaredSignalLike = (SignalLikeDefinition_WithInitializer) signalLikeDefinition;
-						nameElement = typedDeclaredSignalLike.getIdentifier();
-						initializer = typedDeclaredSignalLike.getInitializer();
-						ConstantValue rawValue = formallyConstantExpressionEvaluator.evaluate(typedDeclaredSignalLike.getInitializer());
-						value = processedDataType.convertConstantValueImplicitly(rawValue);
-						if ((value instanceof ConstantValue.Unknown) && !(rawValue instanceof ConstantValue.Unknown)) {
-							errorHandler.onError(typedDeclaredSignalLike.getInitializer(), "cannot convert value of type " + rawValue.getDataType() + " to type " + processedDataType);
-						}
-
 					} else {
+						constant.processInitializer(expressionProcessor);
+						constant.evaluate(new ProcessedExpression.FormallyConstantEvaluationContext(errorHandler));
 
-						errorHandler.onError(signalLikeDefinition, "unknown PSI node");
-						continue;
+						// TODO --- convert value
+						// TODO value = processedDataType.convertConstantValueImplicitly(rawValue);
+						// TODO if ((value instanceof ConstantValue.Unknown) && !(rawValue instanceof ConstantValue.Unknown)) {
+						// TODO 	errorHandler.onError(initializer, "cannot convert value of type " + rawValue.getDataType() + " to type " + processedDataType);
+						// TODO }
 
 					}
-					Constant constant = new Constant(nameElement, dataType, processedDataType, initializer, value);
-					add(constant);
-					result.add(constant);
+					definition = constant;
+				} else if (kind instanceof SignalLikeKind_Signal) {
+					definition = new Signal(nameElement, dataType, processedDataType, initializer);
+				} else if (kind instanceof SignalLikeKind_Register) {
+					definition = new Register(nameElement, dataType, processedDataType, initializer);
+				} else {
+					continue;
 				}
-			} else {
-				for (SignalLikeDefinition signalLikeDefinition : typedImplementationItem.getDefinitions().getAll()) {
-					LeafPsiElement nameElement;
-					ExtendedExpression initializer;
-					if (signalLikeDefinition instanceof SignalLikeDefinition_WithoutInitializer) {
-						SignalLikeDefinition_WithoutInitializer typedDeclaredSignal = (SignalLikeDefinition_WithoutInitializer) signalLikeDefinition;
-						nameElement = typedDeclaredSignal.getIdentifier();
-						initializer = null;
-					} else if (signalLikeDefinition instanceof SignalLikeDefinition_WithInitializer) {
-						SignalLikeDefinition_WithInitializer typedDeclaredSignal = (SignalLikeDefinition_WithInitializer) signalLikeDefinition;
-						nameElement = typedDeclaredSignal.getIdentifier();
-						initializer = typedDeclaredSignal.getInitializer();
-					} else {
-						continue;
-					}
-					SignalLike signalLike = convertSignalLike(typedImplementationItem, nameElement, initializer);
-					if (signalLike != null) {
-						add(signalLike);
-						result.add(signalLike);
-					}
-				}
+
+				// add the definition both to the current result and the definition map
+				add(definition);
+				result.add(definition);
+
 			}
 		} else if (implementationItem instanceof ImplementationItem_ModuleInstance) {
 			ModuleInstance moduleInstance = new ModuleInstance((ImplementationItem_ModuleInstance) implementationItem);
@@ -121,24 +121,6 @@ public final class DefinitionProcessor {
 			result.add(moduleInstance);
 		}
 		return result;
-	}
-
-	@Nullable
-	private SignalLike convertSignalLike(@NotNull ImplementationItem_SignalLikeDefinitionGroup signalLikeDefinitionGroup,
-										 @NotNull LeafPsiElement nameElement,
-										 @Nullable ExtendedExpression initializer) {
-		SignalLikeKind kind = signalLikeDefinitionGroup.getKind();
-		DataType dataType = signalLikeDefinitionGroup.getDataType();
-		ProcessedDataType processedDataType = dataTypeProcessor.processDataType(dataType);
-		if (kind instanceof SignalLikeKind_Constant) {
-			throw new IllegalArgumentException("this method should not be called for constants");
-		} else if (kind instanceof SignalLikeKind_Signal) {
-			return new Signal(nameElement, dataType, processedDataType, initializer);
-		} else if (kind instanceof SignalLikeKind_Register) {
-			return new Register(nameElement, dataType, processedDataType, initializer);
-		} else {
-			return null;
-		}
 	}
 
 	private void add(@NotNull Named element) {
